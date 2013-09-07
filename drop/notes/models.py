ecf4f5ec624118ae78d61f69bb3479efc35c8bff
Rodney Folz <folz@rodneyfolz.com>
2013-09-07 12:11:55 -0700

    Different kinds of notes.

--- drop/notes/models.py
+++ drop/notes/models.py
@@ -5,3 +5,15 @@ from django.contrib.gis.db import models as geo_models
 
 class Note(models.Model):
     point = geo_models.PointField()
+
+    class Meta:
+        abstract = True
+
+class TextNote(Note):
+    text = models.TextField()
+
+class ImageNote(Note):
+    image = models.ImageField(upload_to="images")
+
+class VideoNote(Note):
+    video = models.FileField(upload_to="videos")
